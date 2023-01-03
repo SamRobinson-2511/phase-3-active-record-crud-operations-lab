713863a639fdd4addb6bf8fc13f704661cff8b5f
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2023-01-02 22:57:40 -0600

    commit tree 9fdc69433698b4216c347db9dbeec5b49f29b1c9

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,50 @@
 class Movie < ActiveRecord::Base
 
+    def self.create_with_title title
+        find_or_create_by(title: title)
+    end
+
+    def self.first_movie 
+        first
+    end
+
+    def self.last_movie
+        last
+    end
+
+    def self.movie_count
+        count
+    end
+
+    def self.find_movie_with_id id
+        find(id)
+    end
+
+    def self.find_movie_with_attributes attr
+        find_by(attr)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+
+    def update_with_attributes(attr)
+        update(attr)
+    end
+
+    def self.update_all_titles(title)
+        update_all(title: title)
+    end
+
+    def self.delete_by_id(id)
+        find(id).destroy
+    end
+
+
+    def self.delete_all_movies
+        destroy_all
+    end
+
+   
+
 end
